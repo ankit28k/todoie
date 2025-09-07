970d971ff4ec3caec87c2d5d4608c96c00af239d
Ankit Kumar <ankitkumar@Ankits-MacBook-Air.local>
2025-09-07 13:55:55 +0530

    basic todo app frontend and css

---
+++ src/components/todoitem.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Todoitem = ( ) => {
+  return (
+    <li className="todo-item">
+ 
+      <span>
+        <input type="checkbox" />
+        <span className="todo-item-text ">Sample Todo Item </span>
+      </span>
+
+      <p>...</p>
+    </li>
+  )
+}
+
+export default Todoitem;
